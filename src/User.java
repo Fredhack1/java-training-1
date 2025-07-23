fef65316323892a533b9158de431cb4e618ce96f
Fredhack1 <mirdo2co@gmail.com>
2025-07-24 00:50:33 +0100

    refactoring code

--- src/User.java
+++ src/User.java
@@ -2,17 +2,13 @@ public class User {
     private String name;
     private String email;
 
-    public User(String name, String email) {
+    public User(String name, String email, String password) {
         this.name = name;
         this.email = email;
     }
 
-    public String getName() {
-        return name;
-    }
-    public String getEmail() {
-        return email;
-    }
+    public String getName() { return name; }
+    public String getEmail() { return email;}
 
     public String toString() {
         return "Nom: " + name + ", Email: " + email;
