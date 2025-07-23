b038bd8f2f37b26f1364891f3d2cb7bab0aec0c6
Fredhack1 <mirdo2co@gmail.com>
2025-07-24 00:51:57 +0100

    refactoring code

--- src/User.java
+++ src/User.java
@@ -2,7 +2,7 @@ public class User {
     private String name;
     private String email;
 
-    public User(String name, String email, String password) {
+    public User(String name, String email) {
         this.name = name;
         this.email = email;
     }
